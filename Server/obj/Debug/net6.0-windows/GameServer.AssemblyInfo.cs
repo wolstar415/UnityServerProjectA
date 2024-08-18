fc1b4dbdb427142da8867ba3bcffa38a20ff6c16
wolstar <wolstar415@gmail.com>
2024-08-18 23:13:29 +0900

    2차 수정

--- Server/obj/Debug/net6.0-windows/GameServer.AssemblyInfo.cs
+++ Server/obj/Debug/net6.0-windows/GameServer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9285258e041c24af65d5ed84857bbfaeaf6568a4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c97234010659066420b3c571f42412996d20f969")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
